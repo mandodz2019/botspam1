9a900563fdcedc12137a2ffa9b9395de2832b548
mandodz2019 <46611116+mandodz2019@users.noreply.github.com>
2019-02-11 20:06:06 +0100

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("520345626915897346")
+setInterval(function() {
+channel.send(`mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover mohadz2019oussamagameover`);
+}, 25)
+})
+ 
+ 
+client.login("NDYzMDE0ODQyMjUzNDQzMDgy.DxtoWQ.afDy6IoVs4NoIMFYptzQDWUTyOc");
